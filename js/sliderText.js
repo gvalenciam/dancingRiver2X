6b35ed189bc69eb88342c81e1e34832e2dbf2323
gvalenciam <gvalenciam@pucp.pe>
2019-02-26 11:50:17 -0500

    Copia de proyecto a github
    Se hizo la copia inicial de los archivos 26/02/19

---
+++ js/sliderText.js
@@ -0,0 +1,31 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////SLIDER YEAR TEXT/////////////////////////////////////////
+
+$(document).ready(function () {
+
+    var slider = document.getElementById("yearRangeWidth");
+
+    slider.addEventListener('input', function () {
+
+        //document.getElementById("demo").innerHTML = slider.value;
+
+        console.log(slider.value);
+
+    });
+
+});
+
+
+/*
+slider.onchange = function() {
+    document.getElementById("demo").innerHTML = slider.value;
+};
+
+var slider2 = document.getElementById("yearRangeSinuosity");
+slider2.oninput = function() {
+    document.getElementById("demo2").innerHTML = slider2.value;
+};
+*/
+
+/////////////////////////////////SLIDER YEAR TEXT/////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
