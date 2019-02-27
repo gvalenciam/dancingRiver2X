14e22700e3e0c57bf44d6bf116c86261e207e5f9
gvalenciam <gvalenciam@pucp.pe>
2019-02-26 22:42:38 -0500

    Index code cleaning
    Se ordeno codigo (archivos y directorios) y agrego comentarios a las secciones del html

---
+++ js/index/language_handler_logic.js
@@ -0,0 +1,44 @@
+var set_locale_to = function(locale) {
+
+    if (locale) {
+        $.i18n().locale = locale;
+    }
+    $('body').i18n();
+
+};
+
+var language="en";
+
+$.i18n().load( {
+    'en': './i18n/en.json',
+    'ru': './i18n/ru.json',
+    'es': './i18n/es.json'
+} ).done(function() {
+    set_locale_to(language);
+});
+
+History.Adapter.bind(window, 'statechange', function(){
+    set_locale_to(language);
+});
+
+var flag ="./img/us.png";
+var bandera = document.getElementById("bandera");
+
+function myFunction() {
+
+    var selectedLanguage = document.getElementById("idiom").value;
+
+        if(selectedLanguage === "English"){
+            language = "en";
+            flag="./img/us.png";
+        }
+
+    if(selectedLanguage === "Spanish"){
+        language = "es";
+        flag="./img/es.png";
+    }
+
+    set_locale_to(language);
+    bandera.src = flag;
+
+}
